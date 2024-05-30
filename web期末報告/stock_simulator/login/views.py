9328d3d896bf59bbef4f79e364dfb97440035a6f
Ken <ken20040715@gmail.com>
2024-05-30 14:34:07 +0800

    update

--- web期末報告/stock_simulator/login/views.py
+++ web期末報告/stock_simulator/login/views.py
@@ -1,16 +1,12 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.template import loader
-from django.contrib import auth
-from django.shortcuts import render
-from django.contrib.auth import login
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth.models import User
 
-from login.forms import LoginForm
-
-def login(request):
+def login_view(request):
     login_page = loader.get_template('login.html')
     if request.method == 'GET':
         login_form = LoginForm()
@@ -26,34 +22,31 @@ def login(request):
             password = login_form.cleaned_data['password']
             user = authenticate(username=username, password=password)
             if user is not None:
-                auth.login(request, user)
+                auth_login(request, user)
                 main_page = loader.get_template('main.html')
-                context = {'user': request.user,
-                           'message': 'login ok'}
-                return HttpResponse(login_page.render(context, request))
+                context = {'user': request.user, 'message': 'login ok'}
+                return HttpResponse(main_page.render(context, request))
             else:
                 message = 'Login failed (auth fail)'
         else:                    
-            print ('Login error (login form is not valid)')
+            print('Login error (login form is not valid)')
     else:
-        print ('Error on request (not GET/POST)')
+        print('Error on request (not GET/POST)')
 
-
-def logout(request):
-    auth.logout(request)
+def logout_view(request):
+    auth_logout(request)
     main_html = loader.get_template('main.html')
     context = {'user': request.user}
     return HttpResponse(main_html.render(context, request))
 
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_user(username=username, password=password)
             messages.success(request, f'Account created for {username}!')
-            login(request, user)
-            return redirect('home')
-    else:
-        form = RegisterForm()
-    return render(request, 'login/register.html', {'form': form})
+            return redirect('members:main') 
+        else:
+            messages.error(request, f'Username {username} already exists!')
+    return render(request, 'register.html')
--- web期末報告/stock_simulator/stock_simulator/urls.py
+++ web期末報告/stock_simulator/stock_simulator/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('login.urls')),  
-    path('', include('members.urls')),
+    path('',include('members.urls')),
+    path('login/', include('login.urls')),
 ]
+
