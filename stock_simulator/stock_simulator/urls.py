504f18d199af55a33ca2dca0c6e9661de7adb40f
ken <ken20040715@gmail.com>
2024-06-02 15:26:21 +0800

    aaa

--- stock_simulator/stock_simulator/urls.py
+++ stock_simulator/stock_simulator/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('members.urls')),
-    path('login/', include('login.urls')),
+    path('', include('login.urls')),
 ]
 
